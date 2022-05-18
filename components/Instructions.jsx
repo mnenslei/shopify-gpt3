cb13f45f161cfcaa7417f0249081a403c835b960
Michael Enslein <michael_enslein@michaels-mbp.myfiosgateway.com>
2022-05-18 12:14:13 -0400

    test

--- components/Instructions.jsx
+++ components/Instructions.jsx
@@ -10,7 +10,7 @@ const Instructions = () => {
                 <p>Below you will find a text box. In this text box you may enter a prompt or ask me a question.</p>
                 <p>{`For instance, you may ask me, "What was the motif in Franz Kafka's,`} <em>Metamorphosis</em>?"</p>
                 <p>{`Another example of what you may ask me is "how tall is Michael Jordan?"`}</p>
-                <p>Or ask me to write an essay about Radiohead.</p>
+                <p>Or ask me to write an essay about the best band in the world, Radiohead.</p>
             </div>
             <div>
                 <h3>Go on, ask me something...</h3>
