b87c93e9a5228e29d8ee5ec4a6e84d43ff550527
Michael Enslein <michael_enslein@michaels-mbp.myfiosgateway.com>
2022-05-18 12:15:45 -0400

    test

--- components/Instructions.jsx
+++ components/Instructions.jsx
@@ -8,8 +8,8 @@ const Instructions = () => {
                 <h2>Nice to meet you!</h2>
                 <p>My name is <strong>Coco</strong> and I am an AI genie.</p>
                 <p>Below you will find a text box. In this text box you may enter a prompt or ask me a question.</p>
-                <p>{`For instance, you may ask me, "What was the motif in Franz Kafka's,`} <em>Metamorphosis</em>?"</p>
-                <p>{`Another example of what you may ask me is "how tall is Michael Jordan?"`}</p>
+                <p>{`For instance, you may ask me, what was the motif in Franz Kafka's,`} <em>Metamorphosis</em>?"</p>
+                <p>{`Another example of what you may ask me is, how tall is Michael Jordan?`}</p>
                 <p>Or ask me to write an essay about the best band in the world, Radiohead.</p>
             </div>
             <div>
